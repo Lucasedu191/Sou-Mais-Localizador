4c154bc3f9ef11a4695b5ffd3b075b38c5e7a91d
lucasedu191 <lucasatletico@hotmail.com>
2025-11-02 19:04:06 -0300

    commit para upload do plugin

--- soumais-localizador.php
+++ soumais-localizador.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Sou Mais Localizador
  * Description: Localiza unidades, captura leads e integra com Tecnofit.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Sou Mais
  * Text Domain: soumais-localizador
  * Domain Path: /languages
@@ -15,6 +15,7 @@ define( 'SOUMAIS_LOCATOR_FILE', __FILE__ );
 define( 'SOUMAIS_LOCATOR_PATH', plugin_dir_path( __FILE__ ) );
 define( 'SOUMAIS_LOCATOR_URL', plugin_dir_url( __FILE__ ) );
 
+require_once SOUMAIS_LOCATOR_PATH . 'includes/trait-singleton.php';
 require_once SOUMAIS_LOCATOR_PATH . 'includes/class-plugin.php';
 
 SouMais\Locator\Plugin::instance();
