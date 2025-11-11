8912b66a710657acfa04e8a17719cacd21bbe5f7
lucasedu191 <lucasatletico@hotmail.com>
2025-11-10 21:38:34 -0300

    ajuste card do mobile

--- soumais-localizador.php
+++ soumais-localizador.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Sou Mais Localizador
  * Description: Localiza unidades, captura leads e integra com Tecnofit.
- * Version: 1.0.36
+ * Version: 1.0.37
  * Author: Sou Mais
  * Text Domain: soumais-localizador
  * Domain Path: /languages
@@ -12,7 +12,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'SOUMAIS_LOCATOR_VERSION', '1.0.36' );
+define( 'SOUMAIS_LOCATOR_VERSION', '1.0.37' );
 define( 'SOUMAIS_LOCATOR_FILE', __FILE__ );
 define( 'SOUMAIS_LOCATOR_PATH', plugin_dir_path( __FILE__ ) );
 define( 'SOUMAIS_LOCATOR_URL', plugin_dir_url( __FILE__ ) );
